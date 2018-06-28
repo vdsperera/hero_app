cfc6e3d3d6b9fe84e6ebd398517685eca8609220
Vidumini <vidumini.venusit@gmail.com>
2018-06-28 11:48:51 +0530

    modified hero component for get observable data

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+// import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -29,6 +29,7 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void{
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
