4cd69ec11cfdf12fc535f66690bfee07cbbedc99
Vidumini <vidumini.venusit@gmail.com>
2018-06-28 16:34:18 +0530

    created message service

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable,of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {  
 
-  constructor() { }
+  constructor(private messageSerive: MessageService) { }
 
   getHeroes(): Observable<Hero[]>{
+    this.messageSerive.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -32,4 +32,5 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
   }
+
 }
