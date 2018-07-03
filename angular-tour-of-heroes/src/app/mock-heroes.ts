826cc4ebfb14d0aae9b2b990eafd2b46fc0bebb4
Vidumini <vidumini.venusit@gmail.com>
2018-07-03 18:16:33 +0530

    setup services

--- angular-tour-of-heroes/src/app/mock-heroes.ts
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import {Hero} from './hero';
 
 export const HEROES: Hero[] = [
-    { id: 11, name: 'Roman' },
-    { id: 12, name: 'Goldberg' },
-    { id: 13, name: 'Ray' },
-    { id: 14, name: 'Cena' },
-    { id: 15, name: 'Randy' },
-    { id: 16, name: 'Lesnar' },
-    { id: 17, name: 'Strowman' },
-    { id: 18, name: 'Kali' },
-    { id: 19, name: 'Rock' },
-    { id: 20, name: 'Undertaker' }
+    { id: 11, name: 'Warner' },
+    { id: 12, name: 'Starc' },
+    { id: 13, name: 'Buttler' },
+    { id: 14, name: 'Anderson' },
+    { id: 15, name: 'ABD' },
+    { id: 16, name: 'Kagiso' },
+    { id: 17, name: 'Rashid' },
+    { id: 18, name: 'Kusal' },
+    { id: 19, name: 'Lakmal' },
+    { id: 20, name: 'Williamson' }
 ];
